4731b4f4c9c546761d8d26a7dd24201171e35f12
JeaenP <jpvpvalarezo@gmail.com>
2024-06-20 04:11:55 -0500

    Add migration to create superuser

---
+++ base/migrations/0008_create_superuser.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+
+def create_superuser(apps, schema_editor):
+    from django.contrib.auth.models import User
+    User.objects.create_superuser(
+        username='jeaen',
+        email='jpvpvalarezo@gmail.com',
+        password='jp2583462'
+    )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('your_app', '0007_visitador_remove_visita_visitas_and_more'),  # Cambia esto si tienes migraciones previas
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
